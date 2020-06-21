ecad53f0184e67cc286fd7eb5ab7f950cb634f7e
MrAwwad <omarawwad010@gmail.com>
2020-06-21 13:57:19 +0200

    added google-analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,6 +31,15 @@ module.exports = {
       "I have been playing with react for a little over 2 years after one of my college professors advised me to give it a try and I have been in love with it ever since. After I graduated in july 2019 with a Bachelor's degree in IT, I got a scholarship to pursue a master's degree in management information system. However after only one semester I decided to stop and work full-time instead.",
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID,
+        head: true,
+        anonymize: true,
+        respectDNT: true,
+      },
+    },
     `gatsby-plugin-styled-components`,
     "gatsby-plugin-typescript",
     `gatsby-plugin-react-helmet`,
